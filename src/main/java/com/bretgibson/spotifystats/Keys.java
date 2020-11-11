59e5bcb97640faa03f3e0bbf96f6ed14ca4da476
bret-gibson <bret.gibson@gmail.com>
2020-11-11 00:08:25 -0500

    adding .gitignore

--- src/main/java/com/bretgibson/spotifystats/Keys.java
+++ src/main/java/com/bretgibson/spotifystats/Keys.java
@@ -1,5 +1,6 @@
 package com.bretgibson.spotifystats;
 
+// Spotify Keys
 public enum Keys {
     CLIENT_SECRET("8435d9a4becb4bcaa123647a809b40f4"),
     CLIENT_ID("d6e84ae1d0734c5bbb3f39513bafcf5c");
