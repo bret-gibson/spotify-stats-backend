0a823ee9339cf285c4ee63e23b227d0c5b4dafab
bret-gibson <bret.gibson@gmail.com>
2020-11-10 23:52:36 -0500

    Spotify user auth working

--- src/main/java/com/bretgibson/spotifystats/SpotifystatsApplication.java
+++ src/main/java/com/bretgibson/spotifystats/SpotifystatsApplication.java
@@ -3,6 +3,9 @@ package com.bretgibson.spotifystats;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication (exclude = SecurityAutoConfiguration.class)
 public class SpotifystatsApplication {
@@ -10,5 +13,4 @@ public class SpotifystatsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpotifystatsApplication.class, args);
 	}
-
 }
